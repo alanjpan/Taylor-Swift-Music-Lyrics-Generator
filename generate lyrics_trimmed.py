4f86064953f2c3aa4a79af53c9dfb181b6f6fa66
alanjpan <41685445+alanjpan@users.noreply.github.com>
2019-02-08 10:09:57 -0600

    Update generate lyrics_trimmed.py

--- generate lyrics_trimmed.py
+++ generate lyrics_trimmed.py
@@ -5,7 +5,7 @@ Created on Wed Feb 6 20:41:30 2019
 @author: Alan Jerry Pan, CPA, CSC
 @affiliation: Shanghai Jiaotong University
 
-Program used to generate lyrics based on a three-pronged algorithm of lyric topic, lyric rhyme, and lyric syntax. Uses 100 of Taylor Swift's album songs as its core data set.
+Program used to generate lyrics based on a three-by-three algorithm of lyric topic, lyric rhyme, and lyric syntax on verses, chorus, and transition. Uses 100 of Taylor Swift's album songs as its core data set.
 
 Suggested citation as computer software for reference:
 Pan, Alan J. (2019). Taylor Swift Lyrics Generator [Computer software]. Github repository <https://github.com/alanjpan/Taylor-Swift-Lyrics-Generator>
